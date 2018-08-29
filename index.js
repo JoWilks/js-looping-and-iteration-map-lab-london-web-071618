b9c295a2753a0cbfc3db7166af7a0cbf2a3f1d8b
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-08-29 09:00:08 +0100

    JO

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(collection) {
+  return collection.map( driver => driver.toLowerCase() )
+}
+
+function nameToAttributes(collection) {
+  let obj = []
+  collection.map( driver => {
+    let tempObj = {}
+    let split = driver.split(" ");
+    tempObj["firstName"] = split[0];
+    tempObj["lastName"] = split[1];
+    obj.push(tempObj);
+  })
+  return obj
+}
+
+function attributesToPhrase(collection) {
+  let obj = []
+  collection.map( driver => {
+    let string = `${driver.name} is from ${driver.hometown}`;
+    obj.push(string);
+  })
+  return obj
+}
